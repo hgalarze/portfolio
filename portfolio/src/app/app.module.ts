a02d374de1f216eb249b1c698cc94055f57e58f9
hgalarze <hectorgalarze@gmail.com>
2022-04-07 01:14:22 -0300

    Add Header, Banner, Profile and About sections

--- portfolio/src/app/app.module.ts
+++ portfolio/src/app/app.module.ts
@@ -3,16 +3,37 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FontAwesomeModule, FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { HeaderComponent } from './components/header/header.component';
+import { SocialComponent } from './components/social/social.component';
+import { LoginComponent } from './components/login/login.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { AboutComponent } from './components/about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SocialComponent,
+    LoginComponent,
+    BannerComponent,
+    ProfileComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(library: FaIconLibrary) {
+    library.addIconPacks(fas, far, fab);
+  }
+ }
